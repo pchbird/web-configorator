61999718162cf7762237bbc3288f96a5c282eead
pchbird <61976121+pchbird@users.noreply.github.com>
2020-03-17 13:58:51 -0400

    images
    images

---
+++ CNY01A_v3.172.%d.jpg/keyshot-configurator-imagemapping.js
@@ -0,0 +1,39 @@
+imagemappingdata =
+[
+    {
+        "image": "CNY01A_v3.172.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "18+46"
+        ]
+    },
+    {
+        "image": "CNY01A_v3.172.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "18+50"
+        ]
+    },
+    {
+        "image": "CNY01A_v3.172.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "18+58"
+        ]
+    },
+    {
+        "image": "CNY01A_v3.172.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "18+65"
+        ]
+    }
+];
---
+++ CNY01A_v3.172.%d.jpg/keyshot-configurator-modeldata.js
@@ -0,0 +1,51 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "paint%20metallic%20amber%20%233.png",
+                    "multimaterials": [
+                        "18+46"
+                    ],
+                    "name": "Paint Metallic Amber #3"
+                },
+                {
+                    "icon": "paint%20metallic%20amber%20%231.png",
+                    "multimaterials": [
+                        "18+50"
+                    ],
+                    "name": "Paint Metallic Amber #1"
+                },
+                {
+                    "icon": "paint%20metallic%20amber%20%232.png",
+                    "multimaterials": [
+                        "18+58"
+                    ],
+                    "name": "Paint Metallic Amber #2"
+                },
+                {
+                    "checked": true,
+                    "icon": "paint%20metallic%20amber%20%234.png",
+                    "multimaterials": [
+                        "18+65"
+                    ],
+                    "name": "Paint Metallic Amber #4"
+                }
+            ],
+            "name": "Paint Metallic Amber #3"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Paint Metallic Amber #3"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
